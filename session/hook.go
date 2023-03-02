e376a583af0c64472c307d6453d6a2cd092c1472
Gin <tomygin@outlook.com>
2023-03-02 08:31:06 +0800

    反射实现的hook

--- session/hook.go
+++ session/hook.go
@@ -1,6 +1,10 @@
 package session
 
-import "github.com/tomygin/box/log"
+import (
+	"reflect"
+
+	"github.com/tomygin/box/log"
+)
 
 const (
 	BeforeQuery  = "BeforeQuery"
@@ -15,49 +19,47 @@ const (
 
 // 反射
 
-// func (s *Session) CallMethod(method string, value interface{}) {
-// 	//找到当前表 结构体 的 method 方法
-// 	fm := reflect.ValueOf(s.RefTable().Model).MethodByName(method)
+func (s *Session) CallMethod(method string, value interface{}) {
+	//找到当前表 结构体 的 method 方法
+	fm := reflect.ValueOf(s.RefTable().Model).MethodByName(method)
 
-// 	//如果有自定义结构体就不用表结构体
-// 	if value != nil {
-// 		fm = reflect.ValueOf(value).MethodByName(method)
-// 	}
+	//如果有自定义结构体就不用表结构体
+	if value != nil {
+		fm = reflect.ValueOf(value).MethodByName(method)
+	}
 
-// 	// param := []reflect.Value{reflect.ValueOf(s)}
-// 	param := []reflect.Value{}
+	param := []reflect.Value{reflect.ValueOf(s)}
 
-// 	if fm.IsValid() {
-// 		if v := fm.Call(param); len(v) > 0 {
-// 			if err, ok := v[0].Interface().(error); ok {
-// 				log.Error(err)
-// 			}
-// 		}
-// 	}
-
-// }
-
-// 接口
-
-func (s *Session) CallMethod(method string) {
-	//当前操作的表对象
-	o := s.RefTable().Model
-	switch method {
-	case BeforeQuery:
-		if i, ok := o.(IBeforeQuery); ok {
-			i.BeforeQuery(s)
-		} else {
-			log.Error("接口转换失败")
+	if fm.IsValid() {
+		if v := fm.Call(param); len(v) > 0 {
+			if err, ok := v[0].Interface().(error); ok {
+				log.Error(err)
+			}
 		}
-	default:
-		panic("Unsupported hook method")
 	}
 
 }
 
-type IBeforeQuery interface {
-	BeforeQuery(s *Session) error
-}
+// 接口
+
+// func (s *Session) CallMethod(method string, value interface{}) {
+// 	//当前操作的表对象
+// 	// o := s.RefTable().Model
+// 	o := reflect.ValueOf(value)
+// 	switch method {
+// 	case BeforeQuery:
+// 		if i, ok := o.Interface().(IBeforeQuery); ok {
+// 			i.BeforeQuery(s)
+// 		}
+// 	default:
+// 		panic("Unsupported hook method")
+// 	}
+
+// }
+
+// type IBeforeQuery interface {
+// 	BeforeQuery(s *Session) error
+// }
 
 // type IAfterQuery interface {
 // 	AfterQuery(*Session) error
